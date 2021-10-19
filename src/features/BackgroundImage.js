e4bb25ecb3d6fc48ef03a3c22287341038592b2c
charlieforward9 <62311337+charlieforward9@users.noreply.github.com>
2021-10-19 14:25:54 -0400

    Testing Routing Functionality

--- src/features/BackgroundImage.js
+++ src/features/BackgroundImage.js
@@ -4,9 +4,7 @@ import image from "./background.png";
 function BackgroundImage() {
   return (
     <div className={classes.BackgroundImage}>
-      <h1>
-        UF Association of Computer Engineers
-      </h1>
+      <h1>UF Association of Computer Engineers!</h1>
       <img src={image} alt=""></img>
     </div>
   );
