839a053c619603329410378b9b852803b89c3a8a
marla0530 <65103069+marla0530@users.noreply.github.com>
2021-11-09 19:13:29 -0500

    youtube info to database

--- client/src/features/Youtube.js
+++ client/src/features/Youtube.js
@@ -1,56 +1,65 @@
 import React, { Component } from 'react';
-
+import apis from "../api/index"
 require('dotenv').config();
 const APIKey = process.env.REACT_APP_YOUTUBE_API_KEY //API key
-const channelID = UCGGG_yIpk2LKlmNA498Y8Hg
+const channelID = 'UCGGG_yIpk2LKlmNA498Y8Hg' //Web Channel ID
 const resultMax = 50;
+
 
 const selfURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&maxResults=${resultMax}&order=date&key=${APIKey}`
 
 
+async function updateDatabase(link, title, description) {
+    for (let i = 0; i < title.length; i++) {
+        if (title[i] === "UF ACE") {
+            i++;
+            continue;
+        }
+        await apis.getVideobyTitle(title[i])
+            .then(function(){   // Video is found (already exists in the database)
+                console.log('Video already exists');
+            }).catch(function(){    // Video was not found (does not exist in the database)
+                console.log('Creating database entry')
+                var entry = { title: title[i], description: description[i], link: link[i], tags: [] };
+                apis.createVideo(entry)
+                    .then(function() {
+                        console.log('Successfully added video to database')
+                    })
+                    .catch(function() {
+                        console.log('Video could not be added')
+                    })
+            })
+    }
+}
+
 class Youtube extends Component {
     
-    constructor(props){
-
-        super(props);
-        this.state = {
-            resultYoutube: []
-        };
-        this.clicked = this.clicked.bind(this);
-
-    }
-    
     clicked(){
-
+        
         fetch(selfURL)
             .then((response) => response.json ())
             .then((responseJson) => {
-                const resultYoutube = responseJson.items.map(obj => "https://www.youtube.com/embed/"+obj.id.videoId)
-                this.setState({resultYoutube});
+                console.log(responseJson)
+                const link = responseJson.items.map(obj => "https://www.youtube.com/embed/"+obj.id.videoId)
+                const title = responseJson.items.map(obj => obj.snippet.title)
+                const description = responseJson.items.map(obj => obj.snippet.description)
+
+                updateDatabase(link, title, description);   // Update the database with the retrieved videos
             })
             .catch((error) => {
                 console.log(error);
-            })
 
-    }  
+            })
+    }      
 
     
+    
     render(){
-        
-        //first load & blank array. after click button get data into array.
-        console.log(this.state.resultYoutube);
+          
         return(
             <div className="generalWrapper">
                 <button onClick={this.clicked}>Get Videos</button>
-                {
-                    this.state.resultYoutube.map((link, i) => {
-                        console.log(link);
-                        var iframe = <div key={i} className="videoThumb"><iframe width="560" height="315" src={link} title="video" frameBorder="0" allowFullScreen></iframe></div>
-                        return iframe;
-                    })
-                }
-
-                {this.iframe}
+                      
             </div>
         );
     }
