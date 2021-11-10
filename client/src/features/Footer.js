ef4edbb0d7a2ac43c56afa0334a64552bb6152e8
cameron-keene <74378283+cameron-keene@users.noreply.github.com>
2021-11-09 19:07:08 -0500

    11/9/21

--- client/src/features/Footer.js
+++ client/src/features/Footer.js
@@ -13,35 +13,43 @@ const Footer = () => {
     <footer className={styles.mt_5} bg="dark">
       <Container fluid={true}>
         <Row className="border-top justify-content-between p-3">
-          <Col>
-          <h5>Contact Us</h5>
-              <Row className="mb-3">
-                <Form.Group className="mr-3">
-                  <Form.Label>First Name</Form.Label>
-                  <Form.Control size="sm" type="text"/>
-                </Form.Group>
-                <Form.Group className="ml-3">
-                  <Form.Label>Last Name</Form.Label>
-                  <Form.Control size="sm" type="text"/>
-                </Form.Group>
-              </Row>
-              <Row>
-                <Form.Group>
-                  <Form.Label>Email Address</Form.Label>
-                  <Form.Control size="sm" type="email" placeholder="example@example.com" />
-                </Form.Group> 
-              </Row>
-              <Row>
-                <Form.Group>
-                  <Form.Label className="mr-3">Message</Form.Label>
-                  <Form.Control as="textarea" size="sm" type="text" style={{width: "35vw", height: "100px"}}/>
-                </Form.Group>
-              </Row>
-              <Row>
-                <Button variant="primary" type="submit" className="text-center mb-3">
-              Submit
-                </Button>
-              </Row>
+        <Col>
+            {/*This should be a component*/}
+            <h5>Contact Us</h5>
+            <Row className="mb-3">
+              <Form.Group className="mr-3">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control size="sm" type="text" />
+              </Form.Group>
+              <Form.Group className="ml-3">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                  size="sm"
+                  type="email"
+                  placeholder="example@example.com"
+                />
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group>
+                <Form.Label className="mr-3">Message</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  size="sm"
+                  type="text"
+                  style={{ width: "35vw", height: "100px" }}
+                />
+              </Form.Group>
+            </Row>
+            <Row>
+              <Button
+                variant="primary"
+                type="submit"
+                className="text-center mb-3"
+              >
+                Submit
+              </Button>
+            </Row>
           </Col>
           <Col className="p-0" md={6}>
             <Row>
