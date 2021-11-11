4aa5ae32bb27481479e8e87412e776ebeb717e49
Jake Richard <jakerichard@ufl.edu>
2021-11-10 20:37:12 -0500

    Youtube.js adds non-duplicate videos to database

--- server/models/video-model.js
+++ server/models/video-model.js
@@ -4,11 +4,13 @@ const Schema = mongoose.Schema
 const Video = new Schema(
     {
         title: { type: String, required: true },
-        description: {type: String, required: true},
+        description: {type: String, required: false },
         link: { type: String, required: true },
         tags: { type: [String], required: true }
     },
     { timestamps: true },
 )
+
+Video.index({'$**': 'text'});
 
 module.exports = mongoose.model('video', Video)
