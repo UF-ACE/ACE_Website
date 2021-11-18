87417e164697b61aa587e6498c8d5432daaf482e
Jake Richard <jakerichard@ufl.edu>
2021-11-18 14:20:26 -0500

    Deployment bug fix

--- index.js
+++ index.js
@@ -27,4 +27,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
