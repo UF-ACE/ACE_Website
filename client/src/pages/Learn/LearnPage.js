05c3d5ad8fe2a17b32ae2a3e324a9097a53c4a85
charlieforward9 <62311337+charlieforward9@users.noreply.github.com>
2021-11-21 18:21:36 -0500

    Prettified, description added and no pagination

--- client/src/pages/Learn/LearnPage.js
+++ client/src/pages/Learn/LearnPage.js
@@ -1,59 +1,63 @@
 import VideoThumbnail from "../../features/VideoThumbnail";
 import "./LearnPage.css";
-import api from "../../api/index.js"
-import React, { Component } from 'react'
-import Pagination from 'react-bootstrap/Pagination'
+import api from "../../api/index.js";
+import React, { Component } from "react";
+import Pagination from "react-bootstrap/Pagination";
 
-class LearnPage extends Component{
+class LearnPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        videos: [],
-        isLoading: false
-    }
+      videos: [],
+      isLoading: false,
+    };
   }
 
   componentDidMount = async () => {
-    this.setState({isLoading: true})
+    this.setState({ isLoading: true });
 
-    await api.getVideosbyBlacklist().then(videos => {
+    await api.getVideosbyBlacklist().then((videos) => {
       this.setState({
         videos: videos.data.data,
         isLoading: false,
-      })
-    })
-  }
+      });
+    });
+  };
 
   render() {
     let videos;
     let videoDivs;
 
-    if (!this.state.isLoading && this.state.videos.length !== 0)
-    {
+    if (!this.state.isLoading && this.state.videos.length !== 0) {
       videos = this.state.videos;
-      videoDivs = videos.map((video) =>
-        <div key = {video._id}>
-          <div className = "videos">
-            <VideoThumbnail 
-              embedId = {video.link.substring(video.link.lastIndexOf('/') + 1)} 
-              title = {video.title}  
-              description = {video.description}
-              tags= {video.tags}
+      videoDivs = videos.map((video) => (
+        <div key={video._id}>
+          <div className="videos">
+            <VideoThumbnail
+              embedId={video.link.substring(video.link.lastIndexOf("/") + 1)}
+              title={video.title}
+              description={video.description}
+              tags={video.tags}
             />
           </div>
         </div>
-      )
-    }
-    else
-    {
+      ));
+    } else {
       videos = null;
       videoDivs = null;
     }
 
-    return(
-      <div className = "learnpage">
+    return (
+      <div className="learnpage">
+        <div className="description">
+          <h1>ACE's Content Hub</h1>
+          <h3>
+            GBM's, workshops, speaker sessions, elections, and whatever else ACE
+            hosts and publishes!
+          </h3>
+        </div>
         {videoDivs}
-        <Pagination className = "pagination">
+        {/* <Pagination className = "pagination">
           <Pagination.First />
           <Pagination.Prev />
           <Pagination.Item active>{1}</Pagination.Item>
@@ -62,9 +66,9 @@ class LearnPage extends Component{
           <Pagination.Ellipsis />
           <Pagination.Next />
           <Pagination.Last />
-        </Pagination>
+        </Pagination> */}
       </div>
-    )
+    );
   }
 }
 
