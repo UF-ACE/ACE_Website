53ccaca35f2776110564f41ba3a3b466d06f59fe
Jake Richard <jakerichard@ufl.edu>
2021-11-23 16:35:26 -0500

    Corrections to routing for ID functions.

--- server/routes/sponsor-router.js
+++ server/routes/sponsor-router.js
@@ -6,9 +6,9 @@ const router = express.Router()
 
 router.post('/sponsor', SponsorCtrl.createSponsor)
 router.put('/sponsor/:name', SponsorCtrl.updateSponsorbyName)
-router.put('/sponsor/:id', SponsorCtrl.updateSponsorbyID)
+router.put('/sponsor/id/:id', SponsorCtrl.updateSponsorbyID)
 router.delete('/sponsor/:name', SponsorCtrl.deleteSponsorbyName)
-router.delete('/sponsor/:id', SponsorCtrl.deleteSponsorbyName)
+router.delete('/sponsor/id/:id', SponsorCtrl.deleteSponsorbyID)
 router.get('/sponsor/:name', SponsorCtrl.getSponsorbyName)
 router.get('/sponsors', SponsorCtrl.getSponsors)
 
