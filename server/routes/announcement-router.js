3a4724700dced8b8771b594aa308155241a9b0b3
Andrew Liu <aliu2@ufl.edu>
2022-04-08 12:02:52 -0400

    Fixed router paths

--- server/routes/announcement-router.js
+++ server/routes/announcement-router.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const announcementCtrl = require('../control/announcement-control')
 
-router.post('/announcement/', announcementCtrl.announcementCtrl.createAnnouncement)
-router.put('announcement/:id', announcementCtrl.updateAnnouncement)
-router.delete('announcement/:id', announcementCtrl.deleteAnnouncement)
-router.get('announcement/latest', announcementCtrl.getAnnByLatest)
-router.get('announcement/oldest', announcementCtrl.getAnnByOldest)
+router.post('/announcement', announcementCtrl.createAnnouncement)
+router.put('/announcement/:id', announcementCtrl.updateAnnouncement)
+router.delete('/announcement/:id', announcementCtrl.deleteAnnouncement)
+router.get('/announcement/latest', announcementCtrl.getAnnByLatest)
+router.get('/announcement/oldest', announcementCtrl.getAnnByOldest)
 
 module.exports = router;
